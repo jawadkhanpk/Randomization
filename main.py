d6f1bbcb3094529b0a5b9c2efcc83ba14c532fa8
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-10 18:19:24 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+random_float = random.random() *5    # *5 because to import random number from 0.00.. to 4.99..
+print(random_float)
+
+love_score = random.randint(0, 100)
+print(f"your love score is {love_score}")
+
